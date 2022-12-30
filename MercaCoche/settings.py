f058c80af127024899b7cd37f043e09de63d0e35
migromarj <miguelromeroarjona@gmail.com>
2022-12-30 16:14:27 +0100

    feat(#007): Create card layout for cars, and add list of all cars to index

--- MercaCoche/settings.py
+++ MercaCoche/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'crispy_forms',
     'crispy_bootstrap5',
     'main',
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
